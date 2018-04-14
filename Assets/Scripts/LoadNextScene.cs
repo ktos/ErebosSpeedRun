f7ec739d3c4cf45394cdb2ccc79cfa5f1affc2f2
Marcin Badurowicz <m@badurowicz.net>
2018-04-15 00:44:06 +0200

    Fix slower moving forward with fader

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public GameObject sceneSwitcher;
+
+    void OnTriggerEnter(Collider other)
+    {
+        sceneSwitcher.GetComponent<SceneSwitcher>().fadedLoadScene("Scene01");
+    }
+}
