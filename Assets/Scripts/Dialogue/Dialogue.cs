37fa10b913b50b8497f8ba8c53692b2c38a6201b
Marcin Badurowicz <m@badurowicz.net>
2018-06-03 18:23:11 +0200

    Add basic dialogue work

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dialogue
+{
+    public int Id { get; set; }
+    public IEnumerable<string> Lines { get; set; }
+    public IEnumerable<Dialogue> Children { get; set; }
+    public IEnumerable<Action> Actions { get; set; }
+}
